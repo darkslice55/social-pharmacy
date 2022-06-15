57a04e17e9c28856d2ac7290bb4c54e581bb97b4
mac <mac@MacBook-Pro-mac.local>
2022-06-15 17:41:42 +0300

    db_ready

---
+++ db/models/order.js
@@ -0,0 +1,53 @@
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Order extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ User, Basket }) {
+      Order.belongsTo(User, { foreignKey: 'user_id' });
+      Order.hasMany(Basket, { foreignKey: 'order_id' });
+    }
+  }
+  Order.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      Reflect: {
+        model: 'Users',
+        key: 'id',
+      },
+    },
+    total_price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    is_done: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  }, {
+    sequelize,
+    modelName: 'Order',
+  });
+  return Order;
+};
