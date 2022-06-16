d13c48c15ed7b50089b41dfd4e075b4929dda863
danil <danil88tmn@gmail.com>
2022-06-16 15:28:16 +0300

    Add layout of maun-page

--- views/Good.jsx
+++ views/Good.jsx
@@ -2,13 +2,31 @@ const React = require('react');
 
 function Good({ good }) {
   return (
-    <li className="list-group-item d-flex justify-content-between align-items-center task">
-      {/* Форма для редактирования задачи (выполнено, не выполнено) */}
-      <h2>{good.title}</h2>
-      <img src={`${good.img}`} />
-      <p>{good.description}</p>
-      <span>{good.price}</span>
-    </li>
+    // <li className="list-group-item d-flex justify-content-between align-items-center task">
+    //   {/* Форма для редактирования задачи (выполнено, не выполнено) */}
+    //   <h2>{good.title}</h2>
+    //   <img src={`${good.img}`} />
+    //   <p>{good.description}</p>
+    //   <span>{good.price}</span>
+    // </li>
+    <div className="col">
+      <div className="card shadow-sm">
+        <img src={`${good.img}`} className="card-img-top" alt="..." />  
+        {/* <svg className="bd-placeholder-img card-img-top" src={`${good.img}`} width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> */}
+
+        <div className="card-body">
+          <h3 className="card-title"> {good.title} </h3>
+          <p className="card-text">{good.description}</p>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="btn-group">
+              <button type="button" className="btn btn-sm btn-outline-secondary">Купить</button>
+              {/* <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button> */}
+            </div>
+            <small className="text-muted">{good.price}</small>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ views/Header.jsx
@@ -0,0 +1,79 @@
+const React = require('react');
+
+const { Context } = require('../middlewares/reactSsr');
+
+function Header() {
+  const { user } = React.useContext(Context);
+
+  return (
+    <header className="header-wrapper">
+      <div className="header-top">
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <div className="header-info"><a href="tel:+1234567890"> +7 123 456 7890 </a></div>
+
+            </div>
+            <div className="col-6">
+              <div className="header-top-nav">
+                <ul>
+                  {user ? (
+                    <>
+                      {/* <a className="btn btn-primary btn-xl" href={`/users/${user.id}`}>{user.fullname}</a>&nbsp;&nbsp;&nbsp;
+                      <a className="btn btn-primary btn-xl" href="/auth/logout">Log out</a> */}
+                      <li><a href={`/users/${user.id}`}>{user.fullname}</a></li>
+                      <li><a href="/auth/logout"> Выйти</a></li>
+                    </>
+                  )
+                    : (
+                      <>
+                        <li><a href="/auth/login">Вход</a></li>
+                        <li><a href="/auth/register">Регистрация</a></li>
+                      </>
+                    )}
+                  {/* <li><a href="/auth/register">Вход</a></li>
+                  <li><a href="/auth/login">Регистрация</a></li> */}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="header-area header-default sticky-header">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-5 col-sm-3 col-md-3 col-lg-2">
+              <div className="header-logo-area">
+                <a href="index.html">
+                  <img className="logo-main" src="logo.svg" alt="Logo" />
+                  <img className="logo-light" src="assets/img/logo.png" alt="Logo" />
+                </a>
+              </div>
+            </div>
+            <div className="col-7 col-sm-9 col-md-9 col-lg-10">
+              <div className="header-align">
+                <div className="header-navigation-area">
+                  <ul className="main-menu nav justify-content-center">
+                    <li className="has-submenu active"><a href="/">Домой</a></li>
+                    <li><a href="/about">О нас</a></li>
+                    <li><a href="/contacts">Контакты</a></li>
+                  </ul>
+                </div>
+                <div className="header-action-area">
+                  <div className="header-action-cart">
+                    <a className="cart-icon" href="javascript:void(0);">
+                      <i className="icofont-shopping-cart" />
+                    </a>
+                  </div>
+                  <button className="btn-menu btn-menu-rs d-xl-none" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+module.exports = Header;
