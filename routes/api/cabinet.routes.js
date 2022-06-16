3d97ca76f08dced58cd38037109bd2d1a4cf398f
Pauolo <Pauolosavery1@gmail.com>
2022-06-16 16:40:36 +0300

    cabinet add Order List

--- routes/api/cabinet.routes.js
+++ routes/api/cabinet.routes.js
@@ -1,32 +1,29 @@
 const usersRouter = require('express').Router();
 const Cabinet = require('../../views/Cabinet');
+const Orders = require('../../views/Order');
+const { Order } = require('../../db/models');
+
 // const Order = require('../../db/models');
 
 usersRouter.get('/:id', async (req, res, next) => {
   const userId = req.session.userId;
-  console.log('<<<--->>> user id:', userId);
+  // console.log('<<<--->>> user id:', userId);
   // try {
   // console.log('req.params:', req.params.id);
 
-  // const orders = await Order.findOne({ where: { user_id: 3 } });
   res.renderComponent(Cabinet);
   // } catch (er) {
   //   next(er);
   // }
 });
 
+usersRouter.get('/:id/orders', async (req, res) => {
+  const orders = await Order.findAll({ where: { user_id: 3 }, raw: true });
+  // console.log('<<<--->>> orders:', orders);
+  // console.log('<<<--->>> orders:', orders);
+
+  res.renderComponent(Orders, { orders });
+  // res.end()
+});
+
 module.exports = usersRouter;
-
-
-// try {
-//   const goods = await Good.findAll({
-//     order: [
-//       ['createdAt', 'DESC'],
-//     ],
-//   });
-//   console.log(goods);
-
-//   res.renderComponent(Main, { goods });
-// } catch (error) {
-//   res.redirect('/error');
-// }
--- views/Order.jsx
+++ views/Order.jsx
@@ -3,19 +3,20 @@ const Layout = require('./Layout');
 const { Context } = require('../middlewares/reactSsr');
 // const Order = require('../db/models/');
 
-function Cabinet() {
+function Order({ orders }) {
   const { user } = React.useContext(Context);
   return (
     <Layout>
       <a className="btn btn-primary btn-xl" href={`/users/${user.id}`}>{user.fullname}</a>&nbsp;&nbsp;&nbsp;
-      <a className="btn btn-primary btn-xl" href="/auth/logout">Log out</a>
+      <a className="btn btn-primary btn-xl" href="/auth/logout">Log out</a>&nbsp;&nbsp;&nbsp;
+      <a className="btn btn-primary btn-xl" href="/">HOME</a>
       <ul className="goods list-group">
-        <h1 className="btn btn-primary btn-xl">Вы модератор</h1>
+        <h1 className="btn btn-primary btn-xl" >Ваш список заказов</h1>
         <br></br>
-        <a className="btn btn-primary btn-xl" href={`/users/${user.id}/orders`}>Вывести список заказов</a>
+        <a className="btn btn-primary btn-xl">{`total_price: ${orders[0].total_price}`}</a>
       </ul>
     </Layout>
   );
 }
 
-module.exports = Cabinet;
+module.exports = Order;
