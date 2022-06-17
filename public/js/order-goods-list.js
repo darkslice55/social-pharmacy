15f229a427fbde94b6cea9e88b93c2de4f3a9fc8
Pauolo <Pauolosavery1@gmail.com>
2022-06-17 11:53:36 +0300

    order list - need fix list button

--- public/js/order-goods-list.js
+++ public/js/order-goods-list.js
@@ -1,30 +1,31 @@
 // handlers для ручки вывода списка товаров по заказу
-const goods = document.querySelector('.buttonOforderList');
-goods.addEventListener('click', async (orderEvent) => {
-  orderEvent.preventDefault();
-  const orderid = orderEvent.target.dataset.id;
-  // alert(orderid);
-  const response = await fetch(`orders/${orderid}/goods`, {
-    method: 'POST',
-    body: JSON.stringify({
-      orderid,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  const result = await response.text();
-  document.querySelector('.buttonOforderList').insertAdjacentHTML('afterend', result);
-});
 
-// const entrieslist = document.querySelector('.entries');
-// entrieslist.addEventListener('click', async (entries) => {
-//   if (entries.target.className.includes('link-delete')) {
-//     console.log('entries', entries.target.closest('.entry').id);
-//     const deleteId = entries.target.closest('.entry').id;
-//     await fetch(`/delete/${deleteId}`, {
-//       method: 'DELETE',
-//     });
-//     entries.target.closest('.entry').remove();
-//   }
-// });
+const goods = document.querySelectorAll('.list-group');
+goods.forEach(element => {
+  element.addEventListener('click', async (orderEvent) => {
+    // orderEvent.preventDefault();
+    console.log('name = ', orderEvent.target.name);
+    const orderid = orderEvent.target.dataset.id;
+    console.log('id = ', orderid);
+
+    if (orderEvent.target.name === 'listFalse') {
+      orderEvent.target.name = 'listTrue';
+      console.log('CLICK IF TO', orderEvent.target.name);
+      const response = await fetch(`/orders/${orderid}/goods`, {
+        method: 'GET',
+      });
+      const result = await response.text();
+      // нужно выбрать текущую кнопку, а не первую
+      document.querySelector('.buttonOforderList').insertAdjacentHTML('afterend', result);
+    } else {
+      // await fetch(`/orders/${orderid}/goods/del`, {
+      //   method: 'DELETE',
+      // });
+      const listToDelete = orderEvent.target.closest(`.listOforderList`);
+      const querySelector = listToDelete.querySelector('.list-group-numbered');
+      console.log('querySelector ====>', querySelector);
+      querySelector.remove();
+      orderEvent.target.name = 'listFalse';
+    };
+  })
+});
--- routes/api/good.routes.js
+++ routes/api/good.routes.js
@@ -21,5 +21,4 @@ goodsRouter
       },
     });
   });
-
 module.exports = goodsRouter;
